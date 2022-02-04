8bad20c4c97b839a9f77378c50c3260193da6f78
BarryPro <1278423697@qq.com>
2022-02-04 16:07:38 +0800

    增加关联日报和月报

--- http/query_notion.py
+++ http/query_notion.py
@@ -1,9 +1,17 @@
 # -*- coding: UTF-8 -*-
 import my_notion
-import json
+import update_bill
+import traceback
+import thread_util
 
 version_date = '2021-05-13'
 token_auth = "secret_phe6WVdTudowSUsErvQn8WXi1VILdE7li7SZ6uvjVAi"
+# 账单明细表
+alipay_database_id = "526f7c6fccc54ff5b468557fce038dce"
+# 最大重试次数
+MAX_RETRY_COUNT = 15
+# 线程池数量-账单行数
+THREAD_POOL_SUM_ROW = 200
 
 
 # 按时间范围查找
@@ -57,14 +65,67 @@ def gen_search_condition_time(start, end, property_name):
         }
 
 
+# 支持超时重试保存微信账单
+def search_processor(query, property_name, database_id, timeout, retry_count):
+    try:
+        if timeout > MAX_RETRY_COUNT:
+            print("超过最大超时时间"+query)
+            return
+        response = my_notion.search(token_auth, timeout, gen_search_condition_title(query, property_name), database_id)
+        code = response.status_code
+        if code != 200:
+            print(query+"code: "+str(code)+"=====>"+response.content.decode()+"\n")
+            search_processor(query, property_name, database_id, timeout + 1, retry_count + 1)
+        else:
+            return response
+    except Exception as e:
+        print(str(query)+"=====>超时，第"+str(retry_count)+"次重试开始"+"\n")
+        search_processor(query, property_name, database_id, timeout+1, retry_count + 1)
+
+
+def update_thread(page_result):
+    properties = page_result['properties']
+    page_id = page_result['id']
+    # 搜索月报
+    month = properties['月份']['formula']['string']
+    month_page = search_processor(month, "月份", "bbc595d50e2b4db3b9a28fb404d2222d", 5, 10)
+    day_id = ""
+    month_id = ""
+    if month_page is not None:
+        if month_page.json()['results'] is not None:
+            month_id = month_page.json()['results'][0]['id']
+            print("month_id: "+month_id)
+
+    # 搜索日报
+    day = properties['日期']['formula']['string']
+    day_page = search_processor(day, "标题", "53029b8eef9e47e0a3ee916f71018c9a", 10, 1)
+    if day_page is not None:
+        if day_page.json()['results'] is not None:
+            day_id = day_page.json()['results'][0]['id']
+            print("day_id: "+day_id)
+
+    # 参数是否合法检查
+    if month_id == "" or day_id == "":
+        return
+    # 设置月份id
+    update_content = update_bill.gen_update_content(alipay_database_id, day_id, month_id)
+    # print(update_content)
+    update_result = update_bill.update_bill(page_id, update_content, 5, 1)
+    print("page_id: "+page_id+","+page_result['url'])
+    print(update_result)
+
+
 if __name__ == '__main__':
-    page = my_notion.search(token_auth, 5, gen_search_condition_time("2022-01-26", "2022-01-27", "交易时间"),
+    page = my_notion.search(token_auth, 10, gen_search_condition_time("2021-12-01", "2021-12-31", "交易时间"),
                             "526f7c6fccc54ff5b468557fce038dce")
     page_json = page.json()
-    for result in page_json['results']:
-        properties = result['properties']
-        print(properties['月份']['formula']['string'])
-        print(properties['日期']['formula']['string'])
+    index = 1
+    results = page_json['results']
+    print("一共"+str(len(results))+"条数据")
+    # print(page_json)
+    thread_util.thread_pool_processor(results, update_thread, THREAD_POOL_SUM_ROW)
+
+
 
 
 
--- http/update_bill.py
+++ http/update_bill.py
@@ -9,13 +9,26 @@ alipay_database_id = "526f7c6fccc54ff5b468557fce038dce"
 day_database_id = "53029b8eef9e47e0a3ee916f71018c9a"
 # 月报ID
 month_database_id = "bbc595d50e2b4db3b9a28fb404d2222d"
+# 最大重试次数
+MAX_RETRY_COUNT = 15
 
 
 # 更新指定page页面内容
-def update_bill(page_id, body):
-    result = my_notion.update_page(token_auth, body, 10, page_id)
-    print(result.content.decode())
-    # print(body)
+def update_bill(page_id, body, timeout, retry_count):
+    try:
+        if timeout > MAX_RETRY_COUNT:
+            print("超过最大超时时间"+page_id)
+            return
+        response = my_notion.update_page(token_auth, body, timeout, page_id)
+        code = response.status_code
+        if code != 200:
+            print(page_id+"code: "+str(code)+"=====>"+response.content.decode()+"\n")
+            update_bill(page_id, body, timeout+1, retry_count + 1)
+        else:
+            return response
+    except Exception as e:
+        print(str(page_id)+"=====>超时，第"+str(retry_count)+"次重试开始"+"\n")
+        update_bill(page_id, body, timeout+1, retry_count + 1)
 
 
 # 生成更新文本内容
@@ -32,7 +45,6 @@ def gen_update_content(database_id, day_id, month_id):
                 ]
             },
             "月报": {
-                "id": "jzIO",
                 "type": "relation",
                 "relation": [
                     {
@@ -46,5 +58,5 @@ def gen_update_content(database_id, day_id, month_id):
 
 if __name__ == '__main__':
     update_bill('ce620594aac14b6badc867aa889cf8b2',
-                gen_update_content(alipay_database_id, "aa51762e-bb28-4d86-b3c2-b4a5837ab780", "6378dd6a-a00e-4b6f-a9de-61c746414498"))
+                gen_update_content(alipay_database_id, "aa51762e-bb28-4d86-b3c2-b4a5837ab780", "6378dd6a-a00e-4b6f-a9de-61c746414498"), 10, 1)
     # print(my_notion.search(token_auth, 10, "2022年1月", month_database_id, "月份").json())
