9d6076bad534a4596beb6c2e7554d455188e5691
BarryPro <1278423697@qq.com>
2022-03-05 23:25:57 +0800

    格式定义完成

--- http/query_notion.py
+++ http/query_notion.py
@@ -128,13 +128,9 @@ def update_thread(page_result):
     print(update_result)
 
 
-def update_thread_dict_4_month_day(days_dict):
-    print(days_dict)
-
-
 def thread_search_day_id(day):
     day_dict = {}
-    day_id = get_id(day, "标题", "53029b8eef9e47e0a3ee916f71018c9a", 10, 3)
+    day_id = get_id(day, "标题", "53029b8eef9e47e0a3ee916f71018c9a", 20, 3)
     if day_id is not None:
         day_dict[day] = day_id
     else:
@@ -166,6 +162,7 @@ def convert_month_day_dict(bills):
             if month_id is None:
                 return "查询月份ID失败，请重新查询"
             month_day_dict["month_id"] = month_id
+    # 填充day_id
     for month in month_day_dict.keys():
         if month == "month_id":
             continue
@@ -173,7 +170,7 @@ def convert_month_day_dict(bills):
         days_dict = \
             thread_util.thread_pool_submit_processor(month_day_dict[month], thread_search_day_id, THREAD_POOL_SUM_ROW)
         days_result = thread_util.thread_pool_feature_result_processor(days_dict)
-        # print(type(days_result))
+        # print(days_result)
         for day in days_result:
             for day_key in day.keys():
                 # print(month_day_dict[month][day_key])
@@ -183,8 +180,32 @@ def convert_month_day_dict(bills):
     return month_day_dict
 
 
+def update_bill_month_day(bill):
+    bill_item = bill.split("#")
+    bill_id = bill_item[0]
+    day_id = bill_item[1]
+    month_id = bill_item[2]
+    # 设置月份id
+    update_content = update_bill.gen_update_content(alipay_database_id, day_id, month_id)
+    # print(update_content)
+    update_result = update_bill.update_bill(bill_id, update_content, 5, 5)
+    print("page_id: " + bill_id + "更新成功")
+    print(update_result)
+
+
+def convert_bill_list_4_month_day(month_day_dict):
+    bill_result = []
+    for month_key in month_day_dict.keys():
+        if month_key != "month_id":
+            for day_key in month_day_map[month_key].keys():
+                for bill in month_day_map[month_key][day_key]["data"]:
+                    bill_item = bill+"#"+month_day_map[month_key][day_key]["day_id"]+"#"+month_day_dict["month_id"]
+                    bill_result.append(bill_item)
+    return bill_result
+
+
 if __name__ == '__main__':
-    page = my_notion.search(token_auth, 15, gen_search_condition_time("2021-03-01", "2021-03-31", "交易时间"),
+    page = my_notion.search(token_auth, 15, gen_search_condition_time("2021-04-16", "2021-04-26", "交易时间"),
                             "526f7c6fccc54ff5b468557fce038dce")
     page_json = page.json()
     index = 1
@@ -195,7 +216,9 @@ if __name__ == '__main__':
 
     # 格式转换dict按月和日做聚合
     month_day_map = convert_month_day_dict(results)
-    print(month_day_map)
+    bills_list = convert_bill_list_4_month_day(month_day_map)
+    print(bills_list)
+    thread_util.thread_pool_processor(bills_list, update_bill_month_day, THREAD_POOL_SUM_ROW)
 
 
 
